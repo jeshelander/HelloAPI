690952f3d680b864ca787c6c1f768e2317887f93
James Shelander <jeshelander@crimson.ua.edu>
2020-01-29 17:51:22 -0600

    Initial Commit

---
+++ HelloAPI/Controllers/HelloController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using HelloAPI.Models;
+
+namespace HelloAPI.Controllers
+{
+    public class HelloController : ApiController
+    {
+        HellotoAll hello = new HellotoAll();
+
+        [HttpGet]
+        public string Hello()
+        {
+            return hello.HelloWorld;
+        }
+
+        [HttpPost]
+        public string Hello(string setGreeting)
+        {
+            hello.HelloWorld = setGreeting;
+            return "New value set.";
+        }
+    }
+}
---
+++ HelloAPI/Models/HellotoAll.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelloAPI.Models
+{
+    public class HellotoAll
+    {
+        public string HelloWorld { get; set; }
+        
+        public HellotoAll ()
+        {
+            HelloWorld = "Hello World!!";
+        }
+    }
+}
